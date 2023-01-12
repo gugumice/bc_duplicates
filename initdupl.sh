111ec4933052f67379e4bccfab890c0b3eb70c43
Gunars Frickauss <gunarsf@gmail.com>
2023-01-12 14:55:30 +0200

    Update initdupl.sh

--- initdupl.sh
+++ initdupl.sh
@@ -3,12 +3,12 @@ if [ ! -e /home/pi ]; then
     echo "Only run this on your pi."
     exit 1
 fi
-systemctl enable kiosk.service
+systemctl enable dupl.service
 systemctl disable firstboot.service
 raspi-config --expand-rootfs > /dev/null
 CURRENT_HOSTNAME=$(cat /proc/sys/kernel/hostname)
 sleep 5
-IPO=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f2);
+IPO=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f4);
 NEW_HOSTNAME="rpi-dupl-"$IPO
 echo $CURRENT_HOSTNAME
 echo $NEW_HOSTNAME
